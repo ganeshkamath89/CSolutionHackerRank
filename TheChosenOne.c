acf36f4cce9eb71830a30a882482bb7c3691adc4
Ganesh Kamath <ganeshkamath89@gmail.com>
2022-01-16 23:20:42 +0530

    Create TheChosenOne.c
    https://www.hackerrank.com/challenges/the-chosen-one/problem
    "The Chosen One"

---
+++ TheChosenOne.c
@@ -0,0 +1,175 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+long solve(long* a, int a_count)
+{
+    for(long i=2; i<a_count; i++)
+    {
+        long divN_1 = 0;
+        for(long j=0; j<a_count; j++)
+        {
+            if (a[j] % i == 0)
+            {
+                divN_1++;
+            }
+        }
+        if (divN_1 == (a_count-1))
+        {
+            return i;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int n = parse_int(ltrim(rtrim(readline())));
+    char** a_temp = split_string(rtrim(readline()));
+
+    long* a = malloc(n * sizeof(long));
+    char* eptr;
+    for (long i = 0; i < n; i++) {
+        long a_item = strtol(*(a_temp + i), &eptr, 10);
+        *(a + i) = a_item;
+    }    
+    long result = solve(a, n);
+    printf("%lu", result);
+    // Write your code here
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
