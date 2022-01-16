99e4427e81a46d9adb85ebdcadfcffa4b51b997f
Ganesh Kamath <ganeshkamath89@gmail.com>
2022-01-16 22:11:50 +0530

    Create MatrixTracing.c
    https://www.hackerrank.com/challenges/matrix-tracing/problem

---
+++ MatrixTracing.c
@@ -0,0 +1,192 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+#define MOD 1000000007
+unsigned long long dp[2*1000001] = {-1};
+unsigned long long fact(unsigned long long n){
+    if (n==0) return dp[n] = 1;
+    if (~dp[n]) return dp[n];
+    return dp[n] = (n%MOD*fact(n-1)%MOD)%MOD;
+}
+unsigned long long fast_exp(unsigned long long  base, unsigned long long exp){
+    base%=MOD;
+    unsigned long long result = 1;
+    while(exp>0){
+        if (exp & 1){
+            result = (result*base)%MOD;
+        }
+        base = (base*base)%MOD;
+        exp>>=1;
+    }
+    return result;
+}
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ */
+
+unsigned long long solve(long n, long m) {
+    return (dp[m+n-2]%MOD * fast_exp(dp[n-1], MOD-2)%MOD * fast_exp(dp[m-1], MOD-2)%MOD )%MOD;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+    char* eptr;
+
+    int t = parse_int(ltrim(rtrim(readline())));
+    memset(dp,-1,sizeof dp);
+    fact(2000000);
+    
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        char** first_multiple_input = split_string(rtrim(readline()));
+        int n = strtoll(*(first_multiple_input + 0), &eptr, 10);
+        int m = strtoll(*(first_multiple_input + 1), &eptr, 10);
+        unsigned long long result = solve(n, m);
+        fprintf(fptr, "%llu\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
