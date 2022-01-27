eb4554b84dd5f0b1a6bb69d577786a5e699988be
Ganesh Kamath <ganeshkamath89@gmail.com>
2022-01-27 20:28:13 +0530

    Update BetweenTwoSets.c
    reduced range of K

--- BetweenTwoSets.c
+++ BetweenTwoSets.c
@@ -27,9 +27,27 @@ int parse_int(char*);
 
 int getTotalX(int a_count, int* a, int b_count, int* b) {
     int cnt = 0, flag = 1;
-    for (int k=1; k<=100; k++)
+    int kMin = a[0];
+    for(int i = 1; i<a_count; i++)
     {
-        flag = 1;
+        if(kMin > a[i])
+        {
+            kMin = a[i];
+        }
+    }
+    
+    int kMax = b[0];    
+    for(int i = 1; i<b_count; i++)
+    {
+        if(kMax < b[i])
+        {
+            kMax = b[i];
+        }
+    }
+    kMax++;
+    for (int k=kMin; k<=kMax; k++)
+    {
+        int flag = 1;
         for (int i=0; i<a_count; i++)
             if (k % a[i] != 0)
                 flag = 0;
