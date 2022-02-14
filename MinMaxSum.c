436c21158b8d0b4df53d6adcd33afa29ffce7c87
Ganesh Kamath <ganeshkamath89@gmail.com>
2022-02-14 11:03:26 +0530

    Create MinMaxSum.c
    https://www.hackerrank.com/challenges/one-week-preparation-kit-mini-max-sum/problem
    "Mini-Max Sum"

---
+++ MinMaxSum.c
@@ -0,0 +1,200 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+typedef long long ll;
+void swap(ll* a, ll* b)
+{
+    ll t = *a;
+    *a = *b;
+    *b = t;
+}
+
+ll partition (ll arr[], ll low, ll high)
+{
+    ll pivot = arr[high];
+    ll i = (low - 1); 
+    for (ll j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(ll arr[], ll low, ll high)
+{
+    if (low < high)
+    {
+        ll pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void miniMaxSum(ll arr_count, ll* arr) {
+    quickSort(arr, 0, arr_count -1 );
+    printf("%llu %llu", arr[0] + arr[1] + arr[2] + arr[3], arr[arr_count -1] +arr[arr_count -2] +arr[arr_count -3] +arr[arr_count -4]);
+}
+
+int main()
+{
+
+    char** arr_temp = split_string(rtrim(readline()));
+    char* eptr;
+
+    ll* arr = malloc(5 * sizeof(ll));
+
+    for (ll i = 0; i < 5; i++) {
+        ll arr_item = strtoll(*(arr_temp + i), &eptr, 10);
+
+        *(arr + i) = arr_item;
+    }
+
+    miniMaxSum(5, arr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
